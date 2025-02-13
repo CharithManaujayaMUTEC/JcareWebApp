729a9aec24488d166a9b34b9e5e4177d90589849
G.C.M. <charithmanujaya1@gmail.com>
2025-02-13 16:12:58 +0530

    Update App.js

--- Frontend/jcaref/src/App.js
+++ Frontend/jcaref/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Pages/Home';
 import React from "react";
 import Dashboard from './Pages/Dashboard';
+import Patient from './Pages/PatientProfile';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/Home" element={<Home />} />
           <Route path="/Dashboard" element={<Dashboard/>} />
+          <Route path="/Patient" element={<Patient/>} />
         </Routes>
       </BrowserRouter>
     </div>
