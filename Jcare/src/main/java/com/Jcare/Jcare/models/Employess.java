b08ecf70635697f685dbc671934a230de67d69ea
G.C.M. <charithmanujaya1@gmail.com>
2025-02-22 10:58:26 +0530

    Update Employess.java

--- Jcare/src/main/java/com/Jcare/Jcare/models/Employess.java
+++ Jcare/src/main/java/com/Jcare/Jcare/models/Employess.java
@@ -1,15 +1,36 @@
 package com.Jcare.Jcare.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "employess")
 public class Employess {
+    @Id
+    private String employeeId;
     private String name;
     private String email;
     private String password;
     private String role;
-    protected String speciality;
+    private String speciality;
     private String department;
     private String phone;
     private String address;
     private String city;
-    private String liscenceNo;
+    private String licenseNo;
     private String dob;
+
+    public void setName(String name) {
+    }
+
+    public void setPassword(String encode) {
+    }
+
+    public void setRole(String user) {
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
