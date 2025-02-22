b16799857df7a1f3d64cd8199d82ce93f3c0175b
G.C.M. <charithmanujaya1@gmail.com>
2025-02-22 10:58:38 +0530

    Update PatientLogRepo.java

--- Jcare/src/main/java/com/Jcare/Jcare/repositories/PatientLogRepo.java
+++ Jcare/src/main/java/com/Jcare/Jcare/repositories/PatientLogRepo.java
@@ -3,22 +3,9 @@ package com.Jcare.Jcare.repositories;
 import com.Jcare.Jcare.models.PatientLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface PatientLogRepo extends MongoRepository<PatientLog, String> {
-    PatientLog findByPatientId(String patientId);
-    PatientLog findByPatientEmail(String patientEmail);
-    PatientLog findByPatientPhone(String patientPhone);
-    PatientLog findByPatientInsuranceId(String patientInsuranceId);
-    PatientLog findByPatientInsuranceEmail(String patientInsuranceEmail);
-    PatientLog findByPatientInsurancePhone(String patientInsurancePhone);
-    PatientLog findByPatientInsurancePolicy(String patientInsurancePolicy);
-    PatientLog findByPatientInsuranceCompany(String patientInsuranceCompany);
-    PatientLog findByPatientInsurance(String patientInsurance);
-    PatientLog findByPatientName(String patientName);
-    PatientLog findByPatientAddress(String patientAddress);
-    PatientLog findByPatientDob(String patientDob);
-    PatientLog findByPatientGender(String patientGender);
-    PatientLog findByPatientBloodGroup(String patientBloodGroup);
-    PatientLog findByPatientHeight(String patientHeight);
-    PatientLog findByPatientWeight(String patientWeight);
+    Optional<PatientLog> findByDepartmentAndIsDischarged(String department, boolean isDischarged);
 
 }
