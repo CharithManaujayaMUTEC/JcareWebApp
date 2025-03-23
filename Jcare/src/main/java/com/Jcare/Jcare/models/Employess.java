e96ffa06ae4c4d99acf5d8797e4a98f954a907f9
G.C.M. <charithmanujaya1@gmail.com>
2025-03-23 22:47:32 +0530

    Update Employess.java

--- Jcare/src/main/java/com/Jcare/Jcare/models/Employess.java
+++ Jcare/src/main/java/com/Jcare/Jcare/models/Employess.java
@@ -1,13 +1,18 @@
 package com.Jcare.Jcare.models;
 
 import lombok.Data;
+import lombok.Generated;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
 
 @Data
 @Document(collection = "employess")
 public class Employess {
     @Id
+    private String id;
     private String employeeId;
     private String name;
     private String email;
@@ -21,16 +26,48 @@ public class Employess {
     private String licenseNo;
     private String dob;
 
+    public Employess() {
+        this.id = UUID.randomUUID().toString(); // Auto-generate unique ID
+    }
+
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setPassword(String encode) {
+        this.password = encode;
     }
 
     public void setRole(String user) {
+        this.role = user;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        if (employeeId == null || employeeId.isEmpty()) {
+            this.employeeId = UUID.randomUUID().toString(); // Assign a random unique ID
+        } else {
+            this.employeeId = employeeId;
+        }
+    }
+
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
