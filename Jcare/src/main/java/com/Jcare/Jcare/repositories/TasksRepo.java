489a1cc09bd5e5eae8edde1bf331f8d152a41e23
G.C.M. <charithmanujaya1@gmail.com>
2025-03-23 22:47:47 +0530

    Update TasksRepo.java

--- Jcare/src/main/java/com/Jcare/Jcare/repositories/TasksRepo.java
+++ Jcare/src/main/java/com/Jcare/Jcare/repositories/TasksRepo.java
@@ -3,10 +3,12 @@ package com.Jcare.Jcare.repositories;
 import com.Jcare.Jcare.models.Tasks;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TasksRepo extends MongoRepository<Tasks, String> {
     Optional<Tasks> findByTaskDate(String taskDate);
 
-    Optional<Tasks> findByTaskAssignedToIDAndTaskDate(String taskAssignedToID, String taskDate);
+    List<Tasks> findByTaskAssignedToIDAndTaskDate(String taskAssignedToID, String taskDate);
+
 }
