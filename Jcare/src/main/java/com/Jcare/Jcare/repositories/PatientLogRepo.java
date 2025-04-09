2c83036ec454769bd039e94e0a403c13a5ffade9
G.C.M. <charithmanujaya1@gmail.com>
2025-04-10 02:13:25 +0530

    Update PatientLogRepo.java

--- Jcare/src/main/java/com/Jcare/Jcare/repositories/PatientLogRepo.java
+++ Jcare/src/main/java/com/Jcare/Jcare/repositories/PatientLogRepo.java
@@ -3,9 +3,10 @@ package com.Jcare.Jcare.repositories;
 import com.Jcare.Jcare.models.PatientLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PatientLogRepo extends MongoRepository<PatientLog, String> {
     Optional<PatientLog> findByDepartmentAndIsDischarged(String department, boolean isDischarged);
-
+    Optional<PatientLog> findByPatientId(String patientId);
 }
